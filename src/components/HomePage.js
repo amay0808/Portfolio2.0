e98a23cef49de31993b8aa5b5fa94921b12c1af7
Adrian Mayfield <amay0808@protonmail.com>
2024-04-22 01:49:04 -0700

    Refactor CSS and update JSX structure
    - Updated CSS selectors to target classes instead of ids
    - Added 'skill-item' class to each list item in the skills list
    - Corrected JSX structure by ensuring all elements have corresponding closing tags
    - Fixed CSS syntax error in 'border-bottom' property
    - Adjusted margins in '.skills-section' and '.skills-list' for better layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,13 +4,15 @@ import "../components/HomePage.css";
 function HomePage() {
   return (
     <div>
-      <div className="ovals"></div>
-      <div className="ovals"></div>
-      <div className="ovals"></div>
-      <div className="ovals"></div>
-      <div className="ovals"></div>
-      <div id="name">
-        <p className="my-name">adrianmayfield</p>
+      <div classname="oval-design">
+        <div className="ovals"></div>
+        <div className="ovals"></div>
+        <div className="ovals"></div>
+        <div className="ovals"></div>
+        <div className="ovals"></div>
+      </div>
+      <div className="bio-section">
+        <p id="my-name">adrianmayfield</p>
         <div>
           <img
             src={require("../assets/images/profile-pic.webp")}
@@ -20,13 +22,13 @@ function HomePage() {
         </div>
       </div>
       <div className="bio">
-        <p>Nice to meet you!</p>
+        <p id="greetings">Nice to meet you!</p>
         <p>
           I'm
-          <span className="highlight-name"> Adrian Mayfield.</span>
+          <span id="highlight-name"> Adrian.</span>
         </p>
         <div className="bio-details">
-          <p className="description">
+          <p id="description">
             Based in California's Central Valley, I'm a FullStack Developer
             dedicated to crafting user-friendly web applications.
           </p>
@@ -34,51 +36,51 @@ function HomePage() {
         <button>Contact me</button>
       </div>
 
-      <section id="skills">
+      <section className="skills-section">
         <h2>Skills</h2>
         <ul className="skills-list">
-          <li>
+          <li className="skill-item">
             <h3>HTML</h3>
             <p>3 Years Experience</p>
           </li>
-          <li>
+          <li className="skill-item">
             <h3>CSS</h3>
             <p>3 Years Experience</p>
           </li>
-          <li>
+          <li className="skill-item">
             <h3>JavaScript</h3>
             <p>2 Years Experience</p>
           </li>
-          <li>
+          <li className="skill-item">
             <h3>React</h3>
             <p>2 Years Experience</p>
           </li>
-          <li>
+          <li className="skill-item">
             <h3>Node</h3>
             <p>2 Years Experience</p>
           </li>
-          <li>
+          <li className="skill-item">
             <h3>MongoDB</h3>
             <p>2 Years Experience</p>
           </li>
-          <li>
+          <li className="skill-item">
             <h3>Express</h3>
             <p>2 Years Experience</p>
           </li>
-          <li>
+          <li className="skill-item">
             <h3>Python</h3>
             <p>1 Years Experience</p>
           </li>
         </ul>
       </section>
-      <section id="projects">
+      <section className="projects">
         <h2>Projects</h2>
         <img
           src={require("../assets/images/project-around-the-us.png")}
           alt="Project thumbnail"
           className="project-image"
         />
-        <h3>Around the U.S</h3>
+        <li>Around the U.S</li>
         <p>HTML CSS JavaScript</p>
         <button>View project</button>
         <button>View code</button>
@@ -87,7 +89,7 @@ function HomePage() {
           alt="Project thumbnail"
           className="project-image"
         />
-        <h3>Around the U.S </h3>
+        <li>Around the U.S </li>
         <p>HTML CSS JavaScript</p>
         <button>View project</button>
         <button>View code</button>
@@ -96,13 +98,13 @@ function HomePage() {
           alt="Project thumbnail"
           className="project-image"
         />
-        <h3>Around the U.S </h3>
+        <li>Around the U.S </li>
         <p>HTML CSS JavaScript</p>
         <button>View project</button>
         <button>View code</button>
       </section>
 
-      <section id="contact">
+      <section className="contact">
         <h2>Contact</h2>
         <p>
           I would love to hear about your project and how I could help. Please
