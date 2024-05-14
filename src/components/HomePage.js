654507f8ad99484e02791266e7658164fd9b18fb
Adrian Mayfield <amay0808@protonmail.com>
2024-05-14 03:43:06 -0700

    integrate formspree, create contactForm component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import ContactForm from "../components/ContactForm.js";
 import "../components/HomePage.css";
 import PatternRings from "../assets/images/pattern-rings.svg";
 import ProfilePic from "../assets/images/profile-pic.webp";
@@ -11,43 +11,8 @@ import Project1 from "../assets/images/project1.png";
 import Project2 from "../assets/images/project2.png";
 import Project3 from "../assets/images/project3.png";
 import Project4 from "../assets/images/project4.png";
-import Error from "../components/Error.js";
+
 function HomePage() {
-  const [isNameInvalid, setNameInvalid] = useState(false);
-  const [isEmailInvalid, setEmailInvalid] = useState(false);
-  const [isFormSubmissionInvalid, setFormSubmissionInvalid] = useState(false);
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const handleNameChange = (event) => {
-    const name = event.target.value;
-    setUserName(name);
-    const isInvalid = !name.match(/^[A-Za-z ]*$/);
-    setNameInvalid(isInvalid);
-  };
-
-  const handleEmailChange = (event) => {
-    const email = event.target.value;
-    setEmail(email); // set the email state
-    if (email === "") {
-      setEmailInvalid(false);
-    } else {
-      const isInvalid = !email.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/);
-      setEmailInvalid(isInvalid);
-    }
-  };
-  const validateForm = (event) => {
-    event.preventDefault();
-    const email = event.target.elements.email.value;
-    const message = event.target.elements.message.value;
-
-    if (!email || !message || !email.includes("@")) {
-      setFormSubmissionInvalid(true);
-    } else {
-      setFormSubmissionInvalid(false);
-      // submit form
-    }
-  };
-
   return (
     <div className="top-page">
       <p className="my-name">adrianmayfield</p>
@@ -249,102 +214,7 @@ function HomePage() {
               />
             </div>
           </div>
-          <div className="form">
-            <form onSubmit={validateForm} noValidate>
-              <label className="form_label">
-                <input 
-                  className={`form_input name-input ${
-                    isNameInvalid ? "form_input_invalid" : ""
-                  } ${
-                    !isNameInvalid && userName !== "" ? "form_input_valid" : ""
-                  }`}
-                  // className="form_input name-input"
-                  type="text"
-                  name="name"
-                  placeholder="NAME"
-                  required
-                  pattern="[A-Za-z]*"
-                  onChange={handleNameChange}
-                />
-                <Error show={isNameInvalid} message="Invalid character input" />
-                <Error
-                  show={isFormSubmissionInvalid}
-                  message="Form submission error"
-                />
-              </label>
-              <label className="form_label">
-                <input
-                  className={`form_input email-input ${
-                    isEmailInvalid ? "form_input_invalid" : ""
-                  } ${
-                    !isEmailInvalid && email !== "" ? "form_input_valid" : ""
-                  }`}
-                  type="email"
-                  name="email"
-                  placeholder="EMAIL"
-                  required
-                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                  onChange={handleEmailChange}
-                />
-                <Error
-                  show={isEmailInvalid}
-                  message="Invalid character input"
-                />
-                <Error
-                  show={isFormSubmissionInvalid}
-                  message="Form submission error"
-                />
-              </label>
-              <label className="form_label">
-                <textarea
-                  className="form_input message-box"
-                  name="message"
-                  placeholder="MESSAGE"
-                  required
-                ></textarea>
-              </label>
-              <input
-                className="send-message-btn"
-                type="submit"
-                value="SEND MESSAGE"
-              />
-            </form>
-          </div>
-          <div className="wrapper">
-            <p className="name">adrianmayfield</p>
-            <div className="my-name-contact">
-              <div className="social-icons-footer">
-                <a
-                  href="https://www.frontendmentor.io/profile/amay0808"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={Icon1} alt="Frontend Mentor" />
-                </a>
-                <a
-                  href="https://github.com/amay0808"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={Icon2} alt="Github" />
-                </a>
-                <a
-                  href="https://twitter.com/amay0808"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={Icon3} alt="Twitter" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/adrianmayfield"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={Icon4} alt="Linkedin" />
-                </a>
-              </div>
-            </div>
-          </div>
+          <ContactForm />
         </section>
       </div>
     </div>
